8f3bd5dca940727813b16719f0a71ccd6a928343
Hilmawan Zaky <hilmawanzaky57@gmail.com>
2021-11-18 20:55:19 +0700

    add move to trash and restore content

--- src/controllers/content_controllers.js
+++ src/controllers/content_controllers.js
@@ -21,6 +21,50 @@ module.exports = {
             return res.status(500).send({ message: error.message })
         }
     },
+    moveToTrash: async (req, res) => {
+        const { content_id } = req.params
+        const msc = await connection.promise().getConnection()
+        try {
+            let sql = `select * from content where content_id = ?`
+            let [doesExist] = await msc.query(sql, content_id)
+            if (!doesExist.length) {
+                msc.release()
+                return res.status(200).send([])
+            }
+            sql = `update content set ? where content_id = ?`
+            const editData = {
+                is_deleted: 1
+            }
+            await msc.query(sql, [editData, content_id])
+            msc.release()
+            return res.status(200).send({ message: "added to trash" })
+        } catch (error) {
+            msc.release()
+            return res.status(500).send({ message: error.message })
+        }
+    },
+    restoreContent: async (req, res) => {
+        const { content_id } = req.params
+        const msc = await connection.promise().getConnection()
+        try {
+            let sql = `select * from content where content_id = ?`
+            let [doesExist] = await msc.query(sql, content_id)
+            if (!doesExist.length) {
+                msc.release()
+                return res.status(200).send([])
+            }
+            sql = `update content set ? where content_id = ?`
+            const editData = {
+                is_deleted: 0
+            }
+            await msc.query(sql, [editData, content_id])
+            msc.release()
+            return res.status(200).send({ message: "restore success" })
+        } catch (error) {
+            msc.release()
+            return res.status(500).send({ message: error.message })
+        }
+    },
     deleteContent: async (req, res) => {
         const { content_id } = req.params
         const msc = await connection.promise().getConnection()
@@ -31,8 +75,11 @@ module.exports = {
                 msc.release()
                 return res.status(200).send([])
             }
-            sql = `delete from content where content_id = ?`
-            await msc.query(sql, content_id)
+            sql = `update content set ? where content_id = ?`
+            const editData = {
+                is_deleted: 2
+            }
+            await msc.query(sql, [editData, content_id])
             msc.release()
             return res.status(200).send({ message: "delete success" })
         } catch (error) {
@@ -69,6 +116,19 @@ module.exports = {
         try {
             let sql = `select * from content where user_id = ?`
             let [result] = await msc.query(sql, user_id)
+            msc.release()
+            return res.status(200).send(result)
+        } catch (error) {
+            msc.release()
+            return res.status(500).send({ message: error.message })
+        }
+    },
+    getTrash: async (req, res) => {
+        const { user_id } = req.params
+        const msc = await connection.promise().getConnection()
+        try {
+            let sql = `select * from content where user_id = ? and is_deleted = ?`
+            let [result] = await msc.query(sql, [user_id, 1])
             msc.release()
             return res.status(200).send(result)
         } catch (error) {
--- src/routers/content_routers.js
+++ src/routers/content_routers.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { addContent, deleteContent, editContent, getContents } = require('../controllers/content_controllers');
+const { addContent, deleteContent, editContent, getContents, moveToTrash, restoreContent, getTrash } = require('../controllers/content_controllers');
 const router = express.Router()
 
 router.post("/add/:user_id", addContent)
-router.delete("/delete/:content_id", deleteContent)
+router.get("/movetotrash/:content_id", moveToTrash)
+router.get("/restore/:content_id", restoreContent)
+router.get("/delete/:content_id", deleteContent)
 router.patch("/edit/:content_id", editContent)
 router.get("/get/:user_id", getContents)
+router.get("/trash/:user_id", getTrash)
 
 module.exports = router
